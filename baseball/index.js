1bc2fd7b56cb04700addcbabc6e9a3677b3bf9b9
Kang si yeon <rkdtldus0115@naver.com>
2025-05-30 15:13:48 +0900

    선수 삭제 API 추가 + Prettier

--- baseball/index.js
+++ baseball/index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("node-dev/baseball API");
@@ -19,28 +19,40 @@ app.get("/players", async (req, res) => {
 });
 
 app.post("/players", async (req, res) => {
-    const { name, position, team } = req.body;
-    const newPlayer = await prisma.player.create({
-      data: { name, position, team },
-    });
-    res.status(201).json(newPlayer);
+  const { name, position, team } = req.body;
+  const newPlayer = await prisma.player.create({
+    data: { name, position, team },
   });
+  res.status(201).json(newPlayer);
+});
 
-app.patch("/players/:id", async (req,res) => {
+app.patch("/players/:id", async (req, res) => {
   const playerId = number(req.params.id);
   const data = req.body;
 
-try {
-  const updatedPlayer = await prisma.player.update({
-    where : { id : playerId },
-    data,
-  });
-  res.json(updatedPlayer);
-} catch (error) {
-  res.status(404).json({ error : '선수를 찾을 수 없습니다.'});
-}
+  try {
+    const updatedPlayer = await prisma.player.update({
+      where: { id: playerId },
+      data,
+    });
+    res.json(updatedPlayer);
+  } catch (error) {
+    res.status(404).json({ error: "선수를 찾을 수 없습니다." });
+  }
 });
 
+app.delete("/player/:id", async (req, res) => {
+  const playerId = Number(req.params.id);
+
+  try {
+    await prisma.player.delete({
+      where: { id: playerId },
+    });
+    res.json({ message: "선수가 정상적으로 삭제 되었습니다." });
+  } catch (error) {
+    res.status(404).json({ error: "선수를 찾을 수 없습니다." });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Sever running on http://localhost:${PORT}`);
