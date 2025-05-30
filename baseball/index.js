bac63229ab008f71c469df4fe13164d444023174
Kang si yeon <rkdtldus0115@naver.com>
2025-05-30 15:11:19 +0900

    선수 수정 API 추가

--- baseball/index.js
+++ baseball/index.js
@@ -26,6 +26,22 @@ app.post("/players", async (req, res) => {
     res.status(201).json(newPlayer);
   });
 
+app.patch("/players/:id", async (req,res) => {
+  const playerId = number(req.params.id);
+  const data = req.body;
+
+try {
+  const updatedPlayer = await prisma.player.update({
+    where : { id : playerId },
+    data,
+  });
+  res.json(updatedPlayer);
+} catch (error) {
+  res.status(404).json({ error : '선수를 찾을 수 없습니다.'});
+}
+});
+
+
 app.listen(PORT, () => {
   console.log(`Sever running on http://localhost:${PORT}`);
 });
